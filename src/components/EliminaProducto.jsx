d34f09e66c2eebe7272bef54f4595f64354601a8
monicabarriosg <monicabarrios0750@gmail.com>
2024-07-11 13:25:31 -0600

    depuracion de codigo

--- src/components/EliminaProducto.jsx
+++ src/components/EliminaProducto.jsx
@@ -1,56 +1,85 @@
-// este componente es para que el administrador pueda ver los productos subidos en la pagina 
-//y los pueda eliminar
 import { useState, useEffect } from 'react';
-// importacion del metodo GET y DELETE
-import { getData, deleteData } from '../use';
+import { getData, deleteData, putData } from '../use'; 
+import FormularioEditar from '../components/FormularioEditar'; 
 
-function ProductListWithDelete() {
+function EliminarEditarProductos() {
   const [products, setProducts] = useState([]);
+  const [editingProduct, setEditingProduct] = useState(null);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
         const responseData = await getData("http://localhost:3001/user");
-        // actualiza los datos subidos 
-        setProducts(responseData); 
-        
+        setProducts(responseData);
       } catch (error) {
         console.error('Error fetching products:', error);
       }
     };
 
     fetchProducts();
-    // asegura que useEffect se ejecute solo una vez al montar el componente
-  }, []); 
+  }, []);
 
   const handleDelete = async (id) => {
     try {
       const response = await deleteData("http://localhost:3001/user", id);
       console.log('Product deleted:', response);
 
-      // Actualizar la lista de productos después de eliminar
+
       const updatedProducts = products.filter(product => product.id !== id);
       setProducts(updatedProducts);
     } catch (error) {
       console.error('Error deleting product:', error);
-      // Manejar el error: mostrar un mensaje de error o hacer algo adecuado
+    }
+  };
+
+  const openEditForm = (product) => {
+    setEditingProduct(product);
+  };
+
+  const closeEditForm = () => {
+    setEditingProduct(null);
+  };
+
+  const handleEdit = async (updatedProductData) => {
+    try {
+      const response = await putData("http://localhost:3001/user", editingProduct.id, updatedProductData);
+      console.log('Product updated:', response);
+
+
+      const updatedProducts = products.map(product =>
+        product.id === editingProduct.id ? { ...product, ...updatedProductData } : product
+      );
+      setProducts(updatedProducts);
+      setEditingProduct(null); // Limpiar el estado de edición después de editar
+    } catch (error) {
+      console.error('Error updating product:', error);
     }
   };
 
   return (
     <div>
-      <h2>Product List with Delete</h2>
+      <h2>Product List with Delete and Edit</h2>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
             <strong>{product.name}</strong> - ${product.price}
             <p>{product.description}</p>
             <button onClick={() => handleDelete(product.id)}>Delete</button>
+            <button onClick={() => openEditForm(product)}>Edit</button>
           </li>
         ))}
       </ul>
+
+      {/* Formulario de Edición */}
+      {editingProduct && (
+        <FormularioEditar
+          product={editingProduct}
+          handleEdit={handleEdit}
+          closeEditForm={closeEditForm}
+        />
+      )}
     </div>
   );
 }
 
-export default ProductListWithDelete;
+export default EliminarEditarProductos;
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import  { useState } from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
 const AboutUsModal = () => {
   const [show, setShow] = useState(false);
@@ -19,15 +19,16 @@ const AboutUsModal = () => {
           <Modal.Title>Nuestra mision</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-         <p>
-
-
-
-
-En Monic, nos dedicamos a ofrecer moda contemporánea que inspire confianza y estilo en cada paso que das. <br />
- Nos esforzamos por proporcionar prendas de alta calidad que no solo realcen tu apariencia, <br />
-  sino que también reflejen tu individualidad. Con un compromiso absoluto hacia la sostenibilidad <br />
- y la innovación en el diseño, estamos aquí para vestirte de manera que te empodere y te haga brillar en cada ocasión.</p>
+          <p>
+            En Monic, nos dedicamos a ofrecer moda contemporánea que inspire
+            confianza y estilo en cada paso que das. <br />
+            Nos esforzamos por proporcionar prendas de alta calidad que no solo
+            realcen tu apariencia, <br />
+            sino que también reflejen tu individualidad. Con un compromiso
+            absoluto hacia la sostenibilidad <br />y la innovación en el diseño,
+            estamos aquí para vestirte de manera que te empodere y te haga
+            brillar en cada ocasión.
+          </p>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
